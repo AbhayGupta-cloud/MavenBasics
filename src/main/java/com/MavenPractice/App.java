6c930a2dc63b05ca555182b53ff0164078159e57
AbhayGupta-cloud <akg399798@gmail.com>
2022-02-21 15:18:55 +0530

    Added arithmetic operations in App.java

--- src/main/java/com/MavenPractice/App.java
+++ src/main/java/com/MavenPractice/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        //adding two numbers
+        System.out.println(3+4);
+        //subtracting two numbers
+        System.out.println(4-3);
+        //multiplying two numbers
+        System.out.println(3*4);
     }
 }
